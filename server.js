6260125d1294c152142fbfdfa8278f3e66f45104
Philip lambok <philiplambok71@gmail.com>
2017-09-05 21:14:07 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+let express = require('express');
+let hbs = require('hbs');
+let app = express();
+let fs = require('fs');
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+hbs.registerHelper('getCurrentYear', ()=> new Date().getFullYear());
+app.set('view engine', 'hbs');
+
+// middleware
+app.use((req,res,next)=>{
+
+	let log = `You Log in ${new Date()} with ${req.method}:${req.url}`;
+	console.log( log );
+	fs.appendFile('server.log', log + '\n' , (error) => {
+		if(error)
+			console.log('unable to write file');
+	});
+	next();
+});
+
+app.use((req,res,next) => {
+	res.render('maintance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req,res) => {
+
+	res.render('home.hbs', {
+		titlePage: 'Home Pages'
+	});
+
+});
+
+app.get('/about', (req, res) => {
+
+	res.render('about.hbs',{
+		titlePage: 'About Pages',
+	});
+
+});
+
+app.get('/bad', (req, res) => {
+
+	res.send({
+		errorMessage: "203 Bad Request!"
+	});
+
+});
+
+
+app.listen(3000);
